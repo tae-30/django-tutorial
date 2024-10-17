ed98c75bf28772eed2611ecee129c6beae27efd6
tae-30 <yuut0302@icloud.com>
2024-10-17 11:58:00 +0900

    Fixes #8: おわり

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 from . import views
 
@@ -8,4 +9,4 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
-]
+] + debug_toolbar_urls()
